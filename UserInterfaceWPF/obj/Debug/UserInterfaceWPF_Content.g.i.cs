d95c6ddcc8335e489e92baa8745ef4081d9f4a16
Branislav Selenic <baneselenic@gmail.com>
2021-05-31 15:08:35 +0200

    Code refactoring

--- UserInterfaceWPF/obj/Debug/UserInterfaceWPF_Content.g.i.cs
+++ UserInterfaceWPF/obj/Debug/UserInterfaceWPF_Content.g.i.cs
@@ -8,10 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("distancelearning.context.tt")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("distancelearning.edmx.sql")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("distancelearning.tt")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("modeltest21.context.tt")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("modeltest21.tt")]
 
 
